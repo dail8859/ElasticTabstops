50ad4e19960186e3728221e9a5f3be1ebe758922
dail8859 <dail8859@yahoo.com>
2016-09-05 21:57:01 -0400

    Skip cell columns before modification position
    In alot of cases normal typing/backspacing doesn't cause any cells before the position to change at all. This means determining the cell block doesn't have to look until a line with 0 tabs, only until a minimum number of tabs are contained.

--- src/ElasticTabstops.cpp
+++ src/ElasticTabstops.cpp
@@ -118,7 +118,7 @@ struct et_tabstop {
 	int *widest_width_pix;
 };
 
-static void measure_cells(std::vector<std::vector<et_tabstop>> &grid, int start_line, int end_line = -1) {
+static void measure_cells(std::vector<std::vector<et_tabstop>> &grid, int start_line, int end_line, size_t editted_cell) {
 	int current_pos = call_edit(SCI_POSITIONFROMLINE, start_line);
 	direction which_dir = (start_line <= end_line ? FORWARDS : BACKWARDS);
 
@@ -127,22 +127,29 @@ static void measure_cells(std::vector<std::vector<et_tabstop>> &grid, int start_
 		const int line_end = get_line_end(current_pos);
 		int cell_start = current_pos;
 		bool cell_empty = true;
+		size_t cell_num = 0;
 		std::vector<et_tabstop> grid_line;
 
 		while (current_pos != line_end) {
 			if (current_char == '\t') {
-				int text_width_in_tab = 0;
-				if (!cell_empty) {
-					text_width_in_tab = get_text_width(cell_start, current_pos);
-				}
-				grid_line.push_back({ calc_tab_width(text_width_in_tab), nullptr });
+				if (cell_num >= editted_cell) {
 #ifdef _DEBUG
-				// Highlight the cell
-				call_edit(SCI_SETINDICATORCURRENT, grid_line.size() % DBG_INDICATORS);
-				if (cell_empty) call_edit(SCI_INDICATORFILLRANGE, current_pos, 1);
-				else call_edit(SCI_INDICATORFILLRANGE, cell_start, current_pos - cell_start + 1);
+					// Highlight the cell
+					call_edit(SCI_SETINDICATORCURRENT, grid_line.size() % DBG_INDICATORS);
+					if (cell_empty) call_edit(SCI_INDICATORFILLRANGE, current_pos, 1);
+					else call_edit(SCI_INDICATORFILLRANGE, cell_start, current_pos - cell_start + 1);
 #endif
+					int text_width_in_tab = 0;
+					if (!cell_empty) {
+						text_width_in_tab = get_text_width(cell_start, current_pos);
+					}
+					grid_line.push_back({ calc_tab_width(text_width_in_tab), nullptr });
+				}
+				else {
+					grid_line.push_back({ 0, nullptr });
+				}
 				cell_empty = true;
+				cell_num++;
 			}
 			else {
 				if (cell_empty) {
@@ -155,7 +162,7 @@ static void measure_cells(std::vector<std::vector<et_tabstop>> &grid, int start_
 			current_char = (unsigned char)call_edit(SCI_GETCHARAT, current_pos);
 		}
 
-		if (grid_line.size() == 0 && !(which_dir == FORWARDS && call_edit(SCI_LINEFROMPOSITION, current_pos) <= end_line)) {
+		if (grid_line.size() <= editted_cell && !(which_dir == FORWARDS && call_edit(SCI_LINEFROMPOSITION, current_pos) <= end_line)) {
 			break;
 		}
 
@@ -167,18 +174,17 @@ static void measure_cells(std::vector<std::vector<et_tabstop>> &grid, int start_
 	return;
 }
 
-static void stretch_tabstops(int block_edit_linenum, int block_min_end) {
+static void stretch_tabstops(int block_edit_linenum, int block_min_end, int editted_cell) {
 	std::vector<std::vector<et_tabstop>> grid;
 	size_t max_tabs = 0;
-
 	int block_start_linenum;
 
 	if (block_edit_linenum > 0) {
-		measure_cells(grid, block_edit_linenum - 1, -1);
+		measure_cells(grid, block_edit_linenum - 1, -1, editted_cell);
 		std::reverse(grid.begin(), grid.end());
 	}
 	block_start_linenum = block_edit_linenum - grid.size();
-	measure_cells(grid, block_edit_linenum, block_min_end);
+	measure_cells(grid, block_edit_linenum, block_min_end, editted_cell);
 
 	for (const auto &grid_line : grid) {
 		max_tabs = __max(max_tabs, grid_line.size());
@@ -192,8 +198,8 @@ static void stretch_tabstops(int block_edit_linenum, int block_min_end) {
 	call_edit(SCI_MARKERADD, block_start_linenum + grid.size() - 1, MARK_UNDERLINE);
 #endif
 
-	// find columns blocks and stretch to fit the widest cell
-	for (size_t t = 0; t < max_tabs; t++) {
+	// Find columns blocks and stretch to fit the widest cell
+	for (size_t t = editted_cell; t < max_tabs; t++) {
 		bool starting_new_block = true;
 		int first_line_in_block = 0;
 		int max_width = 0;
@@ -210,21 +216,35 @@ static void stretch_tabstops(int block_edit_linenum, int block_min_end) {
 					grid[first_line_in_block][t].text_width_pix = max_width;
 				}
 			}
-			else  {
+			else {
 				// end column block
 				starting_new_block = true;
 			}
 		}
 	}
 
-	// set tabstops
+	// Anything before the editted cell we can keep because we already know what it is
+	std::vector<int> known_tabstops;
+	int cur_tabstop = 0;
+	for (int i = 0; i < editted_cell; i++) {
+		cur_tabstop = call_edit(SCI_GETNEXTTABSTOP, block_start_linenum, cur_tabstop);
+		known_tabstops.push_back(cur_tabstop);
+	}
+
+	// Set tabstops
 	for (size_t l = 0; l < grid.size(); l++) {
 		int current_line_num = block_start_linenum + l;
 		int acc_tabstop = 0;
 
 		call_edit(SCI_CLEARTABSTOPS, current_line_num);
 
-		for (size_t t = 0; t < grid[l].size(); t++) {
+		// Set any known tabstops
+		for (size_t t = 0; t < known_tabstops.size(); t++) {
+			acc_tabstop = known_tabstops[t];
+			call_edit(SCI_ADDTABSTOP, current_line_num, acc_tabstop);
+		}
+
+		for (size_t t = known_tabstops.size(); t < grid[l].size(); t++) {
 			acc_tabstop += *(grid[l][t].widest_width_pix);
 			call_edit(SCI_ADDTABSTOP, current_line_num, acc_tabstop);
 		}
@@ -251,23 +271,25 @@ void ElasticTabstops_SwitchToScintilla(HWND sci, const Configuration *config) {
 void ElasticTabstops_ComputeEntireDoc() {
 	clear_debug_marks();
 
-	stretch_tabstops(0, call_edit(SCI_GETLINECOUNT));
+	stretch_tabstops(0, call_edit(SCI_GETLINECOUNT), 0);
 }
 
 void ElasticTabstops_OnModify(int start, int end, int linesAdded, const char *text) {
 	clear_debug_marks();
 
+	int editted_cell = 0;
 	// If the modifications happen on a single line and doesnt add/remove tabs, we can do some heuristics to skip some computations
 	if (linesAdded == 0 && strchr(text, '\t') == NULL) {
 		// See if there are any tabs after the inserted/removed text
 		if (get_nof_tabs_between(end, get_line_end(end)) == 0) return;
 
-		// Anything else?
+		// Find which cell was actually changed
+		editted_cell = get_nof_tabs_between(get_line_start(start), start);
 	}
 
 	int block_start_linenum = call_edit(SCI_LINEFROMPOSITION, start);
 
-	stretch_tabstops(block_start_linenum, block_start_linenum + (linesAdded > 0 ? linesAdded : 0));
+	stretch_tabstops(block_start_linenum, block_start_linenum + (linesAdded > 0 ? linesAdded : 0), editted_cell);
 }
 
 void ElasticTabstops_OnReady(HWND sci) {
